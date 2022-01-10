19a0e39072bfad2a0420444be4cd115b38adee90
车小康 <35259787+lionche@users.noreply.github.com>
2022-01-10 17:54:48 +0800

    优化分析用例

--- workline/step6_analysis_testcases.py
+++ workline/step6_analysis_testcases.py
@@ -15,10 +15,21 @@ table_Suspicious_Result = Table_Suspicious_Result()
 
 # crash_list = table_Suspicious_Result.selectIdFromTableFunction("778")
 # print(crash_list)
-crash_list = table_Suspicious_Result.selectErrorTypeFromTableFunction("'crash'")
-Majority_JS_engines_throw_runtime_error_exception_list = table_Suspicious_Result.selectErrorTypeFromTableFunction(
-    "'Majority JS engines throw runtime error/exception'")
-Most_JS_engines_pass_list = table_Suspicious_Result.selectErrorTypeFromTableFunction("'Most JS engines pass'")
+
+def findTypeId(type_name):
+    testcase_list = table_Suspicious_Result.selectErrorTypeFromTableFunction(type_name)
+    testcase_id_set = set()
+    for item in testcase_list:
+        testcase_id_set.add(item[2])
+    print(f"{type_name}类型有{len(testcase_id_set)}个")
+    print(testcase_id_set)
+    # print(crash_testcase_id_set)
+
+
+# crash_list = table_Suspicious_Result.selectErrorTypeFromTableFunction("'crash'")
+# Majority_JS_engines_throw_runtime_error_exception_list = table_Suspicious_Result.selectErrorTypeFromTableFunction(
+#     "'Majority JS engines throw runtime error/exception'")
+# Most_JS_engines_pass_list = table_Suspicious_Result.selectErrorTypeFromTableFunction("'Most JS engines pass'")
 # print("一共有%d条crash" % len(crash_list))
 # print("一共有%d条Majority JS engines throw runtime error/exception" % len(
 #     Majority_JS_engines_throw_runtime_error_exception_list))
@@ -61,31 +72,11 @@ def harness_testcase(testcase):
 
 
 if __name__ == '__main__':
-    # crash_testcase_id_set = set()
-    # for item in crash_list:
-    #     crash_testcase_id_set.add(item[2])
-    #
-    # print(len(crash_testcase_id_set))
-    # print(crash_testcase_id_set)
-    # Majority_JS_engines_throw_runtime_error_exception_id_set = set()
-    # for item in Majority_JS_engines_throw_runtime_error_exception_list:
-    #     Majority_JS_engines_throw_runtime_error_exception_id_set.add(item[2])
-    #
-    # print(len(Majority_JS_engines_throw_runtime_error_exception_id_set))
-    # print(Majority_JS_engines_throw_runtime_error_exception_id_set)
+    # findTypeId("'crash'")
+    # findTypeId("'Pass value *** run error'")
+    # findTypeId("'Most JS engines pass'")
+    # findTypeId("'Majority JS engines throw runtime error/exception'")
+    # findTypeId("'Most JS engines crash'")
 
-    # Most_JS_engines_pass_list_id_set = set()
-    # for item in Most_JS_engines_pass_list:
-    #     Most_JS_engines_pass_list_id_set.add(item[2])
-    #
-    # print(len(Most_JS_engines_pass_list_id_set))
-    # print(Most_JS_engines_pass_list_id_set)
-
-    #
-    # for testcase_id in crash_testcase_id_set:
-    #     interesting_testcase = table_Testcase.selectOneFromTableTestcase(testcase_id)
-    #     harness_testcase(interesting_testcase)
-
-
-    interesting_testcase = table_Testcase.selectOneFromTableTestcase(40475)
+    interesting_testcase = table_Testcase.selectOneFromTableTestcase(31872)
     harness_testcase(interesting_testcase)
