6cb398ef46c1155bed3f48a9933a9f209bca8d6a
车小康 <35259787+lionche@users.noreply.github.com>
2022-03-08 22:17:33 +0800

    增加自动分析系统

--- workline/step7_auto_anlysis.py
+++ workline/step7_auto_anlysis.py
@@ -1,9 +1,14 @@
+from pprint import pprint
+
 from src.studyMysql.Table_Operation import Table_Suspicious_Result
 from workline.table_to_class.Table_Suspicious_Result_Class import Suspicious_Result_Object
 
 table_suspicious_Result = Table_Suspicious_Result()
-unfiltered_list = table_suspicious_Result.selectUnFilteredFromTable_Suspicious_Result()
+# unfiltered_list = table_suspicious_Result.selectUnFilteredFromTable_Suspicious_Result()
+# unfiltered_list = table_suspicious_Result.selectIdFromTable_Suspicious_Result(9)
+unfiltered_list = table_suspicious_Result.selectIdFromTable_Suspicious_Result(1774)
 
 for item in unfiltered_list:
     suspicious_result = Suspicious_Result_Object(item)
+    print('-'*50,suspicious_result.Testcase_id)
     suspicious_result.analysis()
--- workline/table_to_class/Table_Suspicious_Result_Class.py
+++ workline/table_to_class/Table_Suspicious_Result_Class.py
@@ -3,6 +3,11 @@ from pprint import pprint
 from src.studyMysql.Table_Operation import Table_Suspicious_Result, Table_Result
 from workline.table_to_class.Table_Result_Class import Result_Object
 
+import yaml
+
+with open('/root/Comfort_all/workline/filter_info.yml', 'r', encoding='utf-8') as fr:
+    filter_info = yaml.load(fr, Loader=yaml.SafeLoader)['returncode_type']
+
 
 class Suspicious_Result_Object(object):
     def __init__(self, Suspicious_Result_Item):
@@ -13,80 +18,101 @@ class Suspicious_Result_Object(object):
         self.Testbed_id = Suspicious_Result_Item[4]
         self.Remark = Suspicious_Result_Item[5]
         self.Is_filtered = Suspicious_Result_Item[6]
+        self.ResultList, self.Returncode = self.getResultListAndReturnCode()
 
-    def analysis(self):
+    def getResultListAndReturnCode(self):
         """
-        自动分析用例
+        通过testcases id反查
         :return:
         """
         table_Result = Table_Result()
         Result_list = table_Result.selectTestcasesFromTableResult(self.Testcase_id)
-        error_info = ''
+        result_object_list = []
+        returncode = ''
         for item in Result_list:
-            Testcase_Id: str = item[1]
-            Testbed_Id = item[2]
-            Returncode = item[3]
-            Stdout = item[4]
-            Stderr = item[5]
+            result_object = Result_Object(item)
+            returncode += str(result_object.Testbed_Id)
+            returncode += f'({result_object.Returncode})'
+            result_object_list.append(result_object)
+        return result_object_list, returncode
 
-            # print(f'用例id{Testcase_Id},引擎编号{Testbed_Id},返回值{Returncode}')
-            error_info += str(Testbed_Id)
-            error_info += f'({str(Returncode)})'
-
-        import yaml
-
-        with open('/root/Comfort_all/workline/filter_info.yml', 'r', encoding='utf-8') as fr:
-            filter_info = yaml.load(fr, Loader=yaml.SafeLoader)['returncode_type']
-
+    def extractYaml1(self):
+        """
+        通过return code来检索文档
+        :return: 找到的话返回当前return code类，进行下一步检索
+        找不到就返回None
+        """
         for returncode_type in filter_info:
-            # 匹配引擎报错信息
-            if returncode_type['error_info'] == error_info:
-                # pprint(returncode_type)
-                # print(f"{self.Testcase_id}错误代号为{returncode_type['error_info']}")
+            if returncode_type['returncode'] == self.Returncode:
+                return returncode_type
+        else:
+            return None
 
-                for items in returncode_type['error_type']:
-                    try:
-                        stdout_list = items['Stdout']
+    def extractYaml2(self, error_info_list):
+        """
+        检索error info，提取出stdout，和stderr。
+        :return:返回两个list，里面分别是stdout和stderr
+        """
+        flag = True
 
-                        if stdout_list is not None:
-                            for stdout in stdout_list:
-                                self.judgeInfo(stdout, 'stdout')
+        for error_info_item in error_info_list:
+            stdoutList = error_info_item['Stdout']
+            stderrList = error_info_item['Stderr']
+            # print(stdoutList)
+            # print(stderrList)
+            if stdoutList is not None:
+                flag = self.compareInfo('Stdout', stdoutList)
+            if stderrList is not None:
+                flag = self.compareInfo('Stderr', stderrList)
+            if flag:
+                return flag, error_info_item
+        return flag, error_info_item
 
+    def analysis(self):
+        Returncode_block = self.extractYaml1()
+        if Returncode_block:
+            # print(Returncode_block['error_info'])
+            ifAnalysisSuccess, error_info_item = self.extractYaml2(Returncode_block['error_info'])
+            if (ifAnalysisSuccess):
+                print('成果匹配，remark-id为',error_info_item['remark-id'],'错误原因是',error_info_item['remark'])
+            else:
+                print('只匹配到return code',self.Returncode)
+                # todo 进行人工分析
+        else:
+            print('没有匹配到return code')
+            # todo 进行人工分析
 
-                    except:
-                        pass
+    def judgeInfo(self, stdout: str, info: str):
+        if info in stdout:
+            return True
+        else:
+            return False
 
-                    try:
-                        stderr_list = items['Stderr']
-                        if stderr_list is not None:
-                            print(stderr_list)
-                    except:
-                        pass
-                    # print(items['Stderr'])
+    def compareInfo(self, type, stdList):
+        for std in stdList:
+            # print('engine:', std['engine'])
+                # print('info:', std['info'])
+                if type == 'Stdout':
+                    if self.ResultList[std['engine'] - 1].Testbed_Id == std['engine']:
+                        # print("Stdout:", self.ResultList[std['engine'] - 1].Stdout)
+                        if self.judgeInfo(self.ResultList[std['engine'] - 1].Stdout, std['info']):
+                            pass
+                            # print('合格')
+                        else:
+                            # print('不合格')
+                            return False
+                            # break
 
-                # for testcase in testcases:
-                #     result = Result_Object(testcase)
-                #     result.Testbed_Id
+                if type == 'Stderr':
+                    if self.ResultList[std['engine'] - 1].Testbed_Id == std['engine']:
+                        # print('stderr:', self.ResultList[std['engine'] - 1].Stderr)
+                        if self.judgeInfo(self.ResultList[std['engine'] - 1].Stderr, std['info']):
+                            pass
+                            # print('合格')
+                        else:
+                            # print('不合格')
+                            return False
+                            # break
+        return True
 
-    #         #匹配引擎报错信息
-    #         if returncode_type['error_info'] == error_info:
-    #             pprint(returncode_type)
-    #             print(testcase_ids)
-    #             for testcase_id in testcase_ids:
-    #                 print(f"{testcase_id}错误代号为{returncode_type['error_info']}")
-    #             testcase = table_Result.selectTestcasesFromTableResult(testcase_id)
-
-    def judgeInfo(self, stdout, type):
-
-        if type == 'stdout':
-            print(stdout['engine'], stdout['info'])
-
-            for testcase in testcases:
-                result = Result_Object(testcase)
-                if result.Testbed_Id == stdout['engine'] and stdout['info'] in result.Stdout:
-                    print("yes")
-
-        if type == 'stderr':
-            pass
-
-
+        # if type == 'stdout':
