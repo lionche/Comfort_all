52f0c233adf0b31dca73eb82799458436c2193f3
车小康 <35259787+lionche@users.noreply.github.com>
2022-03-14 16:13:12 +0800

    Update initialize.py

--- initialize.py
+++ initialize.py
@@ -17,8 +17,10 @@ class Django_work(object):
         self.runShell('python /root/Comfort_all/web/manage.py runserver 0.0.0.0:10000')
 
     def loaddata(self):
-        self.runShell('python /root/Comfort_all/web/manage.py loaddata initial_data.json')
+        self.runShell('python /root/Comfort_all/web/manage.py loaddata analysis_data.json')
 
+    def backupData(self):
+        self.runShell('python /root/Comfort_all/web/manage.py dumpdata analysis > analysis_data.json')
 
 class Menu(object):
     def __init__(self):
@@ -27,7 +29,8 @@ class Menu(object):
             "1": self.django_work.makemigrationsAndMigrate,
             "2": self.django_work.runserver,
             "3": self.django_work.loaddata,
-            "4": self.quit
+            "4": self.django_work.backupData,
+            "5": self.quit
         }
 
     def display_menu(self):
@@ -36,7 +39,8 @@ class Menu(object):
 1. 同步数据库
 2. 启动web服务器
 3. 恢复差分引擎数据
-4. 退出
+4. 备份数据
+5. 退出
 """)
 
     def run(self):
