e65fe637eb22625efb1898a126141ef67f06ba97
车小康 <35259787+lionche@users.noreply.github.com>
2022-08-04 16:53:19 +0800

    修改输出

--- workline/analysisCov1.py
+++ workline/analysisCov1.py
@@ -141,9 +141,9 @@ def printCovByRaise(raise_rate_dic):
     # print(raise_rate_dic)
     for file_key in raise_rate_dic:
         # print(file_key)
-        print("-" * 30 + f"{file_key}" + "-" * 30)
-        for type_key in raise_rate_dic[file_key]:
-            if raise_rate_dic[file_key][type_key]['old_cov'] and   raise_rate_dic[file_key][type_key]['new_cov']:
+        if raise_rate_dic[file_key]['functions_percent']['old_cov'] and raise_rate_dic[file_key]['functions_percent']['new_cov']:
+            print("-" * 30 + f"{file_key}" + "-" * 30)
+            for type_key in raise_rate_dic[file_key]:
                 print('%s从%.2f%%提高到%.2f%%,提高了%.2f%%' % (
                     type_key, raise_rate_dic[file_key][type_key]['old_cov'],
                     raise_rate_dic[file_key][type_key]['new_cov'],
