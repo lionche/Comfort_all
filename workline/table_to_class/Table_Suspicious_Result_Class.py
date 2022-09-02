584c3790bb13ada03545c9d84168e364f404d597
车小康 <35259787+lionche@users.noreply.github.com>
2022-09-02 17:33:47 +0800

    yw更新代码
    crash过滤系统

--- workline/table_to_class/Table_Suspicious_Result_Class.py
+++ workline/table_to_class/Table_Suspicious_Result_Class.py
@@ -1,3 +1,6 @@
+import os
+import subprocess
+import time
 from pprint import pprint
 
 from workline.mysql_tools.Table_Operation import Table_Testcase, Table_Result, Table_Suspicious_Result
@@ -95,30 +98,35 @@ class Suspicious_Result_Object(object):
         return error_type_list
 
     def analysis(self):
-        Returncode_block = self.extractYaml1()
-        if not Returncode_block:
-            # print('没有匹配到return code')
-            # todo 进行人工分析
-            pass
+        if self.Error_type == "crash" and (self.Testbed_id == 3 or self.Testbed_id == 6 or self.Testbed_id == 9):
+            testcase = self.Code_content
+            self.runWithAsan(testcase)
+
         else:
-            # print('匹配到return code', self.Returncode)
-            error_info_list = self.extractYaml2(Returncode_block['error_info'])
-            if len(error_info_list) == 0:
-                pass
-                # print('info对不上')
+            Returncode_block = self.extractYaml1()
+            if not Returncode_block:
+                # print('没有匹配到return code')
                 # todo 进行人工分析
+                pass
             else:
-                remark_id = ""
-                for error_info_item in error_info_list:
-                    # print(f"remark-id是{error_info_item['remark-id']},错误原因是:{error_info_item['remark']}")
-                    remark_id += f"({str(error_info_item['remark-id'])})"
-                table_suspicious_Result = Table_Suspicious_Result()
-                try:
-                    table_suspicious_Result.updateIs_filtered(self.Id, remark_id)
-                    # print(f'将{remark_id}存入数据库')
-                except:
-                    # print('存入数据库失败')
+                # print('匹配到return code', self.Returncode)
+                error_info_list = self.extractYaml2(Returncode_block['error_info'])
+                if len(error_info_list) == 0:
                     pass
+                    # print('info对不上')
+                    # todo 进行人工分析
+                else:
+                    remark_id = ""
+                    for error_info_item in error_info_list:
+                        # print(f"remark-id是{error_info_item['remark-id']},错误原因是:{error_info_item['remark']}")
+                        remark_id += f"({str(error_info_item['remark-id'])})"
+                    table_suspicious_Result = Table_Suspicious_Result()
+                    try:
+                        table_suspicious_Result.updateIs_filtered(self.Id, remark_id)
+                        # print(f'将{remark_id}存入数据库')
+                    except:
+                        # print('存入数据库失败')
+                        pass
 
     def judgeInfo(self, stdout: str, info: str):
         if info in stdout:
@@ -178,3 +186,59 @@ class Suspicious_Result_Object(object):
 
         # if type == 'stdout':
 
+    #  测试crash过滤
+    # def analyseCrash(self):
+    #     if self.Error_type == "crash" and (self.Testbed_id == 3 or self.Testbed_id == 6 or self.Testbed_id == 9):
+    #         testcase = self.Code_content
+    #         # print(testcase)
+    #         self.runWithAsan(testcase)
+
+    def runWithAsan(self, testcase):
+        timeout = 30
+        testbed_id = self.Testbed_id
+        temp_testcase = os.path.join(os.getcwd(), str(time.time()).replace(".", "") +".js")
+        with open(temp_testcase, "w") as f:
+            f.write(testcase)
+        table_suspicious_Result = Table_Suspicious_Result()
+        if testbed_id == 3:
+            # testbed = "/root/.jsvu/engines/chakra-asan/ChakraCore-1.11.24/out/Release/ch"
+            testbed = "/root/.jsvu/engines/chakra-asan/chakra-1.11.24/ch"
+            cmd = ["timeout", "-s9", str(timeout), testbed, temp_testcase]
+            pro = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE, universal_newlines=True)
+            stdout, stderr = pro.communicate()
+            if(str(stderr).strip().startswith("AddressSanitizer")):
+                remark_id = "asan"
+                table_suspicious_Result.updateIs_filtered(self.Id, remark_id)
+            else:
+                remark_id = "1"
+                table_suspicious_Result.updateIs_filtered(self.Id, remark_id)
+
+        if testbed_id == 9:
+            # testbed = "/root/.jsvu/engines/chakra-asan/ChakraCore/out/Release/ch"
+            testbed = "/root/.jsvu/engines/chakra-asan/chakra-1.13-beta/ch"
+            cmd = ["timeout", "-s9", str(timeout), testbed, temp_testcase]
+            pro = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE, universal_newlines=True)
+            stdout, stderr = pro.communicate()
+            if (str(stderr).strip().startswith("AddressSanitizer")):
+                remark_id = "asan"
+                table_suspicious_Result.updateIs_filtered(self.Id, remark_id)
+            else:
+                remark_id = "1"
+                table_suspicious_Result.updateIs_filtered(self.Id, remark_id)
+        if testbed_id == 6:
+            testbed = "/root/.jsvu/engines/jerryscript-asan/jerryscript-fea10bb7/build/bin/jerry"
+            cmd = ["timeout", "-s9", str(timeout), testbed, temp_testcase]
+            pro = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE, universal_newlines=True)
+            stdout, stderr = pro.communicate()
+            # print("stdout:" + "\n" + stdout)
+            # print("stderr:" + "\n" + stderr)
+            if ("Error" in str(stderr) and not("timeout: the monitored command dumped core" in str(stderr))):
+                remark_id = "1"
+                table_suspicious_Result.updateIs_filtered(self.Id, remark_id)
+            else:
+                remark_id = "asan"
+                table_suspicious_Result.updateIs_filtered(self.Id, remark_id)
+        os.remove(temp_testcase)
